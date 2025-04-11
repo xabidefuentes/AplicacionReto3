00f23ebeb5c294fd44b9ccf8dd91544fcf32264a
OierSM <oiersanmartinruiz@gmail.com>
2025-04-11 12:51:09 +0200

    .

--- src/conexiones/AutorCN.java
+++ src/conexiones/AutorCN.java
@@ -16,20 +16,20 @@ public class AutorCN {
     public static void menuAutores(){
         Io.sop("***********************************************************************");
         Io.sop("************************  GESTION DE AUTORES  ************************");
-        Io.sop("********************  DE LA BIBLIOTECA MUNICIPAL ********************");
-        Io.sop("***************************   DE MUSKIZ  ***************************");
+        Io.sop("*********************  DE LA BIBLIOTECA MUNICIPAL *********************");
+        Io.sop("****************************   DE MUSKIZ  ****************************");
         Io.sop("***********************************************************************");
-        Io.sop("1. AGREGAR AUTOR");
-        Io.sop("2. BORRAR AUTOR");
-        Io.sop("3. MODIFICAR AUTOR");
-        Io.sop("4. SALIR");
+        Io.sop("1. ➕AGREGAR AUTOR");
+        Io.sop("2. ➖BORRAR AUTOR");
+        Io.sop("3. ✏️MODIFICAR AUTOR");
+        Io.sop("4. 🚶‍♂️SALIR");
         int opcion = Io.leerInt("Selecciona una opción: ");
         switch (opcion) {
             case 1:
                 AutorCN.insertarAutor();
                 break;
             case 2:
-                    
+                AutorCN.borrarAutor();        
                 break;
             case 3:
                 
@@ -45,6 +45,46 @@ public class AutorCN {
     //INSERTAR Autor//
 
     public static int insertarAutor(){
+        Connection conn = Io.getConexion();
+        int cambios = 0;
+        int id_autor;
+        String nombre,apellido,nacionalidad;
+        LocalDate fechaNac;
+        nombre = Io.leerString("Dime el nombre del autor: ");//Leemos del teclado
+        apellido = Io.leerString("Dime el apellido del autor: ");//Leemos del teclado
+        nacionalidad = Io.leerString("Dime de donde es/era el autor: ");//Leemos del teclado
+        fechaNac = Io.leerDate("Dime cuando nació el autor: ");//Leemos del teclado
+        //Comprobacion para ver si el id_autor esta repetido
+        do {
+            id_autor = Io.leerInt("Dime el ID del autor que quieras ponerle: ");
+            if (Io.comprobarExistenciaInt(conn, "autores", "id_autor", id_autor)) {
+                Io.sop("ID ya existente, introduce otro");
+            }
+        } while (Io.comprobarExistenciaInt(conn, "autores", "id_autor", id_autor));
+        String sql = "INSERT INTO usuarios (id_autor, nombre, apellido, nacionalidad, fechaNac) " +
+        "VALUES ('" + id_autor + "', '" + nombre + "', '" + apellido + "', '" + nacionalidad + "', '" + fechaNac + "')";       
+        try{
+            Statement st = conn.createStatement();   // es para que se agrege a la bbdd
+            cambios = st.executeUpdate(sql);
+            if(cambios == 0){
+                System.out.println("No se ha añadido el registro");
+            }
+                else{
+                    System.out.println("Registro añadido");
+                }
+        }
+        catch(SQLException e){
+            System.out.println("Problema al insertar la tabla");
+            System.out.println(e.getErrorCode());
+            System.out.println(sql);
+        }
+        return cambios;
+
+    }
+
+    //INSERTAR Autor//
+
+    public static int borrarAutor(){
         Connection conn = Io.getConexion();
         int cambios = 0;
         int id_autor;
